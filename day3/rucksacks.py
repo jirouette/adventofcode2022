fba188ba30e1dd4b9f1eb836a50d15b249013771
jr <jr@jirouette.com>
2022-12-04 01:57:07 +0100

    day3 part2

--- day3/rucksacks.py
+++ day3/rucksacks.py
@@ -2,6 +2,7 @@
 #coding: utf8
 
 Rucksack = tuple[str, str]
+Group = list[str, str, str]
 
 def get_compartments_from_line(line: str) -> Rucksack:
     """Parse the two compartments of a rucksack from a string line."""
@@ -29,12 +30,24 @@ def compute_priority_of_all_rucksacks(rucksacks: list[Rucksack]) -> int:
     """Compute the total priority of all rucksacks."""
     return sum(map(lambda rucksack: get_priority_from_char(get_duplicate_char(rucksack)), rucksacks))
 
+def compute_priority_of_all_rucksack_groups(rucksacks: list[Rucksack]) -> int:
+    """Compute the total priority of all badges of 3-lines groups of Rucksacks."""
+    # we suppose we have groups of exactly 3 lines
+    badges = []
+    for i in range(0, len(rucksacks), 3):
+        group: Group = [compartments[0] + compartments[1] for compartments in rucksacks[i:i+3]] # rebuild whole rucksacks from compartments
+        badge = list(set(group[0]) & set(group[1]) & set(group[2]))[0] # compute badges
+        badges.append(badge)
+    return sum(map(lambda badge: get_priority_from_char(badge), badges))
+
 if __name__ == '__main__':
     with open('puzzle_input_example.txt') as f:
         rucksacks: list[Rucksack] = list(map(lambda line: get_compartments_from_line(line), f.read().split("\n")))
         print("Puzzle input example")
         print("Part 1:", compute_priority_of_all_rucksacks(rucksacks))
+        print("Part 2:", compute_priority_of_all_rucksack_groups(rucksacks))
     with open('puzzle_input.txt') as f:
         rucksacks: list[Rucksack] = list(map(lambda line: get_compartments_from_line(line), f.read().split("\n")))
         print("Puzzle input")
         print("Part 1:", compute_priority_of_all_rucksacks(rucksacks))
+        print("Part 2:", compute_priority_of_all_rucksack_groups(rucksacks))
