6839e1da325495186e59f3b3f1c772ab1e85df1f
jr <jr@jirouette.com>
2022-12-07 16:54:23 +0100

    day7 part2

--- day7/filetree.py
+++ day7/filetree.py
@@ -24,6 +24,11 @@ class Directory:
         """Add a file"""
         self.files[name] = size
     
+    def get_directories(self) -> list["Directory"]:
+        dirs = list(self.directories.values())
+        subdirs = [d for dir in dirs for d in dir.get_directories()]
+        return dirs+subdirs
+    
 def parse_terminal_output(data: str) -> Directory:
     root = Directory()
     working_directory = root
@@ -54,14 +59,30 @@ def part1_get_sum_of_directories_of_a_size(dir: Directory, size: int = 100_000) 
         dir_size = 0
     return dir_size + sum(map(lambda dir: part1_get_sum_of_directories_of_a_size(dir, size), dir.directories.values()))
 
+def part2_find_smallest_directory_to_remove(root: Directory, total_disk_space: int = 70_000_000, free_space_needed: int = 30_000_000) -> int:
+    smallest_size_needed = free_space_needed - (total_disk_space - root.size)
+    if smallest_size_needed <= 0:
+        return 0
+    directories = root.get_directories()
+    directories.sort(key=lambda dir: dir.size)
+    for dir in directories:
+        size = dir.size
+        if size >= smallest_size_needed:
+            return size
+    return root.size
+
 if __name__ == '__main__':
     with open('day7/puzzle_input_example.txt') as f:
         term_output = f.read().strip()
         root = parse_terminal_output(term_output)
         print("Puzzle input example")
         print("Part 1:", part1_get_sum_of_directories_of_a_size(root))
+        print("Part 2:", part2_find_smallest_directory_to_remove(root))
     with open('day7/puzzle_input.txt') as f:
         term_output = f.read().strip()
         root = parse_terminal_output(term_output)
         print("Puzzle input example")
         print("Part 1:", part1_get_sum_of_directories_of_a_size(root))
+        print("Part 2:", part2_find_smallest_directory_to_remove(root))
+
+# Well, a lot of optimisation is possible (caching dir size for example)
