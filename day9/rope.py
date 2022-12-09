2480f6a88d397961fa26f2dec19f591a462d4668
jr <jr@jirouette.com>
2022-12-09 12:16:42 +0100

    day9 part2

--- day9/rope.py
+++ day9/rope.py
@@ -24,9 +24,10 @@ def parse_moves(data: str) -> list[Move]:
     data: list[str] = data.split()
     return [(Direction(direction), int(steps)) for direction, steps in zip(data[::2], data[1::2])]
 
-def apply_move(head: Position, tail: Position, move: Move) -> list[BasicPosition]:
+def apply_move(knots: list[Position], move: Move) -> list[BasicPosition]:
     direction, steps = move
     tail_pos = []
+    head, knots = knots[0], knots[1:]
     while steps:
         steps -= 1
         match direction:
@@ -38,38 +39,51 @@ def apply_move(head: Position, tail: Position, move: Move) -> list[BasicPosition
                 head.x -= 1
             case Direction.RIGHT:
                 head.x += 1
-        if is_tail_far_beyond(head, tail):
-            tail_pos.append(update_tail(head, tail))
+        
+        prev_knot = head
+        last_knot_pos: BasicPosition = (knots[-1].x, knots[-1].y)
+        for knot in knots:
+            if is_knot_far_beyond(prev_knot, knot):
+                update_knot(prev_knot, knot)
+            prev_knot = knot
+            last_knot_pos = (knot.x, knot.y)
+        tail_pos.append(last_knot_pos)
+        #print(direction, steps+1, head, tail)
     return tail_pos
 
-def part1(data: str) -> int:
+def part1(data: str, nb_knots: int = 2) -> int:
     nb_moves = []
-    head = Position(0, 0)
-    tail = Position(0, 0)
-    nb_moves = [pos for move in parse_moves(data) for pos in apply_move(head, tail, move)]
-    return len(set(nb_moves)) + 1 # not forgetting the starting position
+    knots = [Position(0, 0) for _ in range(nb_knots)]
+    nb_moves = [pos for move in parse_moves(data) for pos in apply_move(knots, move)]
+    return len(set(nb_moves))
 
-def is_tail_far_beyond(head: Position, tail: Position) -> bool:
-    return abs(head.x - tail.x) > 1 or abs(head.y - tail.y) > 1
+def part2(data: str) -> int:
+    return part1(data, nb_knots=10)
 
-def update_tail(head: Position, tail: Position) -> BasicPosition:
-    if tail.x == head.x:
-        tail.y += head.y > tail.y
-        tail.y -= head.y < tail.y
-    elif tail.y == head.y:
-        tail.x += head.x > tail.x
-        tail.x -= head.x < tail.x
+def is_knot_far_beyond(knot1: Position, knot2: Position) -> bool:
+    return abs(knot1.x - knot2.x) > 1 or abs(knot1.y - knot2.y) > 1
+
+def update_knot(knot1: Position, knot2: Position):
+    if knot2.x == knot1.x:
+        knot2.y += knot1.y > knot2.y
+        knot2.y -= knot1.y < knot2.y
+    elif knot2.y == knot1.y:
+        knot2.x += knot1.x > knot2.x
+        knot2.x -= knot1.x < knot2.x
     else:
-        tail.x += head.x > tail.x
-        tail.x -= head.x < tail.x
-        tail.y += head.y > tail.y
-        tail.y -= head.y < tail.y
-    return tail.x, tail.y
+        knot2.x += knot1.x > knot2.x
+        knot2.x -= knot1.x < knot2.x
+        knot2.y += knot1.y > knot2.y
+        knot2.y -= knot1.y < knot2.y
 
 if __name__ == '__main__':
     with open('day9/puzzle_input_example.txt') as f:
         print("Puzzle input example")
-        print("Part 1:", part1(f.read()))
+        data = f.read()
+        print("Part 1:", part1(data))
+        print("Part 2:", part2(data))
     with open('day9/puzzle_input.txt') as f:
         print("Puzzle input")
-        print("Part 1:", part1(f.read()))
+        data = f.read()
+        print("Part 1:", part1(data))
+        print("Part 2:", part2(data))
