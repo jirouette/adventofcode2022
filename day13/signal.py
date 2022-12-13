9d4638ecf60d525be76790df45eb5f4182c17c5e
jr <jr@jirouette.com>
2022-12-14 00:46:06 +0100

    day13 part2

--- day13/signal.py
+++ day13/signal.py
@@ -2,21 +2,21 @@
 #coding: utf8
 
 import json
+from functools import cmp_to_key
 
-Packet = list[int|list]
-Pair = tuple[Packet, Packet]
-
-def parse_pairs(filename: str) -> list[Pair]:
+def parse_packets(filename: str) -> list:
     data = open(filename).read().strip()
-    return [tuple([json.loads(l) for l in pair.split("\n")]) for pair in data.split("\n\n")]
+    return [json.loads(l) for l in data.replace("\n\n", "\n").split("\n")]
 
-def check_pair(p1: Pair, p2: Pair) -> bool:
+def check_pair(p1, p2) -> bool:
     if not p1 and p2:
         return True
     if p1 and not p2:
         return False
     if not p1 and not p2:
         return None
+    p1 = p1.copy()
+    p2 = p2.copy()
     left = p1.pop(0)
     right = p2.pop(0)
     if type(left) is int and type(right) is int:
@@ -32,16 +32,26 @@ def check_pair(p1: Pair, p2: Pair) -> bool:
         return result
     return check_pair(p1, p2)
 
-def count_right_orders(pairs: list[Pair]) -> int:
+def count_right_orders(pairs: list) -> int:
     return sum([i+1 for i in range(len(pairs)) if check_pair(*pairs[i]) != False])
+
+def sort_packets(packets: list) -> list:
+    return sorted(packets, key=cmp_to_key(lambda p1, p2: {True: -1, None: 0, False: 1}[check_pair(p1, p2)]))
+
+def decoder_key(packets: list, p1, p2) -> int:
+    return (packets.index(p1)+1) * (packets.index(p2)+1)
+
+DIVIDER_PACKET = [[[2]], [[6]]]
 
 if __name__ == '__main__':
     print("Puzzle input example")
     filename = "day13/puzzle_input_example.txt"
-    pairs = parse_pairs(filename)
-    print(count_right_orders(pairs))
+    pairs = parse_packets(filename)
+    print(count_right_orders([(p1, p2) for p1, p2 in zip(pairs[::2], pairs[1::2])]))
+    print(decoder_key(sort_packets(pairs+DIVIDER_PACKET), *DIVIDER_PACKET))
 
     print("Puzzle input")
     filename = "day13/puzzle_input.txt"
-    pairs = parse_pairs(filename)
-    print(count_right_orders(pairs))
+    pairs = parse_packets(filename)
+    print(count_right_orders([(p1, p2) for p1, p2 in zip(pairs[::2], pairs[1::2])]))
+    print(decoder_key(sort_packets(pairs+DIVIDER_PACKET), *DIVIDER_PACKET))
